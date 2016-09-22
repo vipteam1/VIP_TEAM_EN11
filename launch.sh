1aa5956c9c38d2b57dffcdf8eb8e4a0d84d8fead
vipteam1 <vipteam1@users.noreply.github.com>
2016-09-22 14:04:09 +0300

    Add files via upload

--- launch.sh
+++ launch.sh
@@ -13,7 +13,7 @@ update() {
 install_luarocks() {
   git clone https://github.com/keplerproject/luarocks.git
   cd luarocks
-  git checkout tags/v2.3.0-rc2 # Release Candidate
+  git checkout tags/v2.2.1 # Current stable
 
   PREFIX="$THIS_DIR/.luarocks"
 
@@ -38,22 +38,17 @@ install_rocks() {
     then echo "Error. Exiting."; exit $RET;
   fi
 
-  ./.luarocks/bin/luarocks install lbase64 20120807-3
+ ./.luarocks/bin/luarocks install lbase64 20120807-3
+ RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+  
+ ./.luarocks/bin/luarocks install luasocket
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
 
-  ./.luarocks/bin/luarocks install luafilesystem
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install lub
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install luaexpat
+  ./.luarocks/bin/luarocks install oauth
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
@@ -125,22 +120,6 @@ else
     echo "Run $0 install"
     exit 1
   fi
-  
-  chmod 777 devpoint.sh
-  
-  
-   echo -e "\033[38;5;208m"
-   echo -e "      ____  __  __    _    ____ _____ _____ _____    _    __  __ "
-   echo -e "     / ___||  \/  |  / \  |  _ \_   _|_   _| ____|  / \  |  \/  |"
-   echo -e "     \___ \| |\/| | / _ \ | |_) || |   | | |  _|   / _ \ | |\/| |"
-   echo -e "      ___) | |  | |/ ___ \|  _ < | |   | | | |___ / ___ \| |  | |"
-   echo -e "     |____/|_|  |_/_/   \_\_| \_\|_|___|_| |_____/_/   \_\_|  |_|"
-   echo -e "                                  |_____|                        "
-   echo -e "     > Channel : @Smart_team                        "
-   echo -e "     > Developer : @lua_3 - @jozef3                       "
-   echo -e "     > Bot ID : @Help_telp                      "
-   echo -e "     > Github : github.com/team-smart/smart_team     "
-   echo -e "                                              \033[0;00m"
-   echo -e "\e[36m"
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/smartbot.lua -l 1 -E $@
+  rm -r ../.telegram-cli/state #Prevent tg from crash
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/TH3BOSS.lua -l 1 -E $@
 fi
